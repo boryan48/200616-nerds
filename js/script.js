2384255a7725e353a3a752ebe0fe40c5bedd4116
boryan48 <boryan48@mail.ru>
2016-05-10 11:54:09 +0300

    Рабочий скрипт

--- js/script.js
+++ js/script.js
@@ -1,40 +1,20 @@
 var link = document.querySelector(".form-popup");
-var popup = document.querySelector(".write-us");
-var close = popup.querySelector(".btn-close");
-var login = popup.querySelector("[name=text]");
-var form = popup.querySelector("form");
-var email = popup.querySelector("[name=email]");
+var popup = document.querySelector(".popup");
+var shadow = document.querySelector(".overlay");
+var close = document.querySelector(".btn-close");
+var name = popup.querySelector("[name=text]");
 
 link.addEventListener("click", function (event) {
   event.preventDefault();
-  popup.classList.add("popup-content-show");
-  login.focus();
+  popup.classList.add("popup-show");
+  shadow.classList.add("overlay-show");
+  name.focus();
 });
 
 close.addEventListener("click", function (event) {
   event.preventDefault();
-  popup.classList.remove("popup-content-show");
-  popup.classList.remove("popup-error");
+  popup.classList.remove("popup-show");
+  shadow.classList.remove("overlay-show");
 });
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-});
 
-form.addEventListener("submit", function (event) {
-  if (!Text.value || !email.value) {
-    event.preventDefault();
-    popup.classList.remove("popup-error");
-    popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("popup-error");
-  }
-});
-
-window.addEventListener("keydown", function (event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains(".popup-content-show")) {
-      popup.classList.remove("popup-content-show");
-      popup.classList.remove("popup-error");
-    }
-  }
-});
