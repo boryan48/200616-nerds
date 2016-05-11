a0b301592a9a37d217f45ab207ed62f08e631c93
Keks <keks@htmlacademy.ru>
2016-05-11 12:30:52 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+var link = document.querySelector(".form-popup");
+var popup = document.querySelector(".popup");
+var shadow = document.querySelector(".overlay");
+var close = document.querySelector(".btn-close");
+var name = popup.querySelector("[name=text]");
+
+link.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.classList.add("popup-show");
+  shadow.classList.add("overlay-show");
+  name.focus();
+});
+
+close.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.classList.remove("popup-show");
+  shadow.classList.remove("overlay-show");
+});
+
+
